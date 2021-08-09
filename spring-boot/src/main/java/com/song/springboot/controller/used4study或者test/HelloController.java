b397358903d7821b45e87bbae4406e3158bb06bf
szx <3055217083@qq.com>
2021-08-09 13:57:13 +0800

    update

--- spring-boot/src/main/java/com/song/springboot/controller/used4study或者test/HelloController.java
+++ spring-boot/src/main/java/com/song/springboot/controller/used4study或者test/HelloController.java
@@ -1,6 +1,5 @@
 package com.song.springboot.controller.used4study或者test;
 
-import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.song.springboot.AAA_TWO2222222222.燃えろ俺の小宇宙よ.NameEnum;
 import com.song.springboot.entity.learn.Book;
 import com.song.springboot.service.learn.HelloService;
@@ -10,18 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.security.RolesAllowed;
-import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 
 /**
  * @author : zhixian.song
@@ -35,13 +28,11 @@ public class HelloController {
 
     @Autowired
     HelloService helloService;
-    @Autowired
-    DefaultKaptcha defaultKaptcha;
 
     @ResponseBody
     @GetMapping(value = {"/in", "/f"})
     Book getHello() throws Exception {
-        if (false) {
+        if (true) {
             NameEnum nameEnum = NameEnum.SONG_ZHI_XIAN;
             throw new Exception("12321");
         }
@@ -66,7 +57,7 @@ public class HelloController {
 
     @GetMapping(value = {"/nihao"})
     String goToHTML() {
-        return "html/used4studyORtest/hello";
+        return "html/used4studyORtest/hello2";
     }
 
     @GetMapping("/error/404")
@@ -77,54 +68,5 @@ public class HelloController {
     @GetMapping("/error/500")
     public String error500() {
         return "templates/error/50X";
-    }
-
-    /**
-     * yan zheng ma , bu tai xing de yang zi
-     * 验证码,不太行的样子
-     * @params :
-     * @returns :
-     */
-    @RequestMapping("/defaultKaptcha")
-    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        byte[] captchaChallengeAsJpeg = null;
-        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
-        try {
-            //生产验证码字符串并保存到session中
-            String createText = defaultKaptcha.createText();
-            httpServletRequest.getSession().setAttribute("vrifyCode", createText);
-            //使用生产的验证码字符串返回一个BufferedImage对象并转为byte写入到byte数组中
-            BufferedImage challenge = defaultKaptcha.createImage(createText);
-            ImageIO.write(challenge, "jpg", jpegOutputStream);
-        } catch (IllegalArgumentException e) {
-            httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-        //定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
-        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
-        httpServletResponse.setHeader("Cache-Control", "no-store");
-        httpServletResponse.setHeader("Pragma", "no-cache");
-        httpServletResponse.setDateHeader("Expires", 0);
-        httpServletResponse.setContentType("image/jpeg");
-        ServletOutputStream responseOutputStream =
-                httpServletResponse.getOutputStream();
-        responseOutputStream.write(captchaChallengeAsJpeg);
-        responseOutputStream.flush();
-        responseOutputStream.close();
-    }
-
-    @RequestMapping("/imgvrifyControllerDefaultKaptcha")
-    public String imgvrifyControllerDefaultKaptcha(HttpServletRequest httpServletRequest, Model model) {
-        String captchaId = (String) httpServletRequest.getSession().getAttribute("vrifyCode");
-        String parameter = httpServletRequest.getParameter("vrifyCode");
-        System.out.println("Session  vrifyCode " + captchaId + " form vrifyCode " + parameter);
-
-        if (!captchaId.equals(parameter)) {
-            model.addAttribute("info", "错误的验证码");
-            log.error("错误的验证码");
-        } else {
-            model.addAttribute("info", "登录成功");
-        }
-        return "html/used4studyORtest/hello";
     }
 }
