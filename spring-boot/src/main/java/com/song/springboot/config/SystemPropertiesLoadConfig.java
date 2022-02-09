fea75c07756fa6dc6ae4c45734dd7d0599f7bd1c
szx <3055217083@qq.com>
2022-02-09 10:09:21 +0800

    update

--- spring-boot/src/main/java/com/song/springboot/config/SystemPropertiesLoadConfig.java
+++ spring-boot/src/main/java/com/song/springboot/config/SystemPropertiesLoadConfig.java
@@ -14,6 +14,7 @@ import java.util.Properties;
 
 /**
  * 配置容器
+ * @author zhixian.song
  * @params :
  * @returns :
  */
--- spring-boot/src/main/java/com/song/springboot/utils/ProjectPropertiesUtil.java
+++ spring-boot/src/main/java/com/song/springboot/utils/ProjectPropertiesUtil.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 
 /**
  * 配置容器
+ * @author zhixian.song
  * @params :
  * @returns :
  */
